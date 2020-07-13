ead0e794e2c2f6028a16fe0864aa79d68574ccde
yourally69@gmail.com <jbkhome69>
2020-07-13 09:14:36 +0300

    feature: переход на 10 ангулар

--- projects/ng-project-helper/src/lib/isOnChanges.ts
+++
@@ -1,6 +0,0 @@
-import { SimpleChange } from '@angular/core';
-
-
-export function isOnChange(change: SimpleChange): boolean {
-  return change && change.currentValue && change.currentValue !== change.previousValue;
-}
