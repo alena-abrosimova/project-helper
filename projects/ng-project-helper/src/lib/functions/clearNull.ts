8a9b2f65fddf7f774180e7022076f00b536499cd
aaahrenovskaya <yourally69@gmail.com>
2020-09-08 16:18:45 +0300

    feature: доработка clearNull

--- projects/ng-project-helper/src/lib/functions/clearNull.ts
+++ projects/ng-project-helper/src/lib/functions/clearNull.ts
@@ -1,7 +1,7 @@
 export function clearNull(clearingObject: object): object {
   const newObject = clearingObject;
   Object.keys(newObject).forEach(key => {
-    if (!newObject[key]) {
+    if (!newObject[key] && newObject[key] !== false || Array.isArray(newObject[key]) && !newObject[key].length) {
       delete newObject[key];
     }
   });
