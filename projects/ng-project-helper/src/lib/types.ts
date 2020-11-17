3eb326d35b36dabfe3c8b46adfb169c80f2b6d4c
aaahrenovskaya <yourally69@gmail.com>
2020-11-17 13:06:19 +0300

    feature: update to 0.1.14 - добавлен KeysOfType

--- projects/ng-project-helper/src/lib/types.ts
+++ projects/ng-project-helper/src/lib/types.ts
@@ -5,3 +5,5 @@ import { UrlTree } from '@angular/router';
 export type Id = number;
 
 export type CanActivateResult = Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree;
+
+export type KeysOfType<T, U> = { [k in keyof T]: T[k] extends U ? k : never }[keyof T];
