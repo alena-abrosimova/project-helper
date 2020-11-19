32ed10c6b1ff0c0030d8ef38244a0dff62a3a46b
aaahrenovskaya <yourally69@gmail.com>
2020-11-19 14:23:00 +0300

    feature: update to 0.1.27 - Partial<T> для simple-http.service.ts

--- projects/ng-project-helper/src/lib/servises/simple-http.service.ts
+++ projects/ng-project-helper/src/lib/servises/simple-http.service.ts
@@ -25,11 +25,11 @@ export class SimpleHttpService {
     return this.http.get<T>(getUrl(url, id, path), {params: generateQuery(params)});
   }
 
-  post<T>(url: string, item: T, path?: string): Observable<T> {
+  post<T>(url: string, item: Partial<T>, path?: string): Observable<T> {
     return this.http.post<T>(getUrl(url, null, path), item);
   }
 
-  patch<T>(url: string, id: number | string, item: T, path?: string): Observable<T> {
+  patch<T>(url: string, id: number | string, item: Partial<T>, path?: string): Observable<T> {
     return this.http.patch<T>(getUrl(url, id, path), item);
   }
 
