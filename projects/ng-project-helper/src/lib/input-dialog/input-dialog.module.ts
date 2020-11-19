fa72d2dbb1dca87fd9b1c72695aa46312c5a5163
aaahrenovskaya <yourally69@gmail.com>
2020-11-19 11:24:51 +0300

    feature: update to 0.1.24 - matInput для input-dialog

--- projects/ng-project-helper/src/lib/input-dialog/input-dialog.module.ts
+++ projects/ng-project-helper/src/lib/input-dialog/input-dialog.module.ts
@@ -7,19 +7,21 @@ import { FlexModule } from '@angular/flex-layout';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [InputDialogComponent],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatCardModule,
-    FlexModule,
-    MatDialogModule,
-    MatButtonModule,
-    FormsModule
-  ],
+    imports: [
+        CommonModule,
+        MatFormFieldModule,
+        MatCardModule,
+        FlexModule,
+        MatDialogModule,
+        MatButtonModule,
+        FormsModule,
+        MatInputModule
+    ],
   exports: [InputDialogComponent]
 })
 export class InputDialogModule {
