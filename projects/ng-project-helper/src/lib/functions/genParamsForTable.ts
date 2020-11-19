dd1295d7c3f5140e6c69f459e8c12b4824dfb7de
aaahrenovskaya <yourally69@gmail.com>
2020-11-19 10:55:06 +0300

    feature: update to 0.1.21 -  доработан genParamsForTable.ts

--- projects/ng-project-helper/src/lib/functions/genParamsForTable.ts
+++ projects/ng-project-helper/src/lib/functions/genParamsForTable.ts
@@ -2,18 +2,15 @@ import { dateToString } from './dateToString';
 import { isDate } from 'date-fns';
 
 
-export function genParamsForTable<T>(params: T, forExport: boolean = false): T {
-  const newParams = Object.assign({}, params, params['paginationOption']);
+export function genParamsForTable<T>(params: T, withoutPagination: boolean = false): T {
+  const newParams = withoutPagination ?
+    Object.assign({}, params) : Object.assign({}, params, params['paginationOption']);
 
   Object.keys(newParams).forEach(key => {
     if (isDate(newParams[key])) {
       newParams[key] = dateToString(newParams[key], 'yyyy-MM-dd');
     }
   });
-
-  if (forExport) {
-    delete newParams['limit'];
-  }
 
   delete newParams['length'];
   delete newParams['paginationOption'];
