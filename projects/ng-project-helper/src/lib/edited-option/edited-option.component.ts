530435ee7c04364fe78f4549ee50b726500dbc6f
aaahrenovskaya <yourally69@gmail.com>
2020-12-18 09:35:30 +0300

    feature: update to 0.2.31 откат изменений

--- projects/ng-project-helper/src/lib/edited-option/edited-option.component.ts
+++ projects/ng-project-helper/src/lib/edited-option/edited-option.component.ts
@@ -2,18 +2,14 @@ import { Component, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { CustomOption } from '../classes/customOption';
-import { MatOption } from '@angular/material/core';
 
 
 @Component({
   selector: 'edited-option',
   templateUrl: './edited-option.component.html',
-  providers: [
-    {provide: CustomOption, useExisting: EditedOptionComponent},
-    {provide: MatOption, useExisting: EditedOptionComponent},
-  ]
+  providers: [{provide: CustomOption, useExisting: EditedOptionComponent}]
 })
-export class EditedOptionComponent<T> extends MatOption implements CustomOption<T> {
+export class EditedOptionComponent<T> implements CustomOption<T> {
   @Input() value: T;
 
   itemSubject: Subject<T> = new Subject<T>();
