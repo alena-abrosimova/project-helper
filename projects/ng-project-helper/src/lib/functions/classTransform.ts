a9e53fc66b592e736e21278457fb392b4a20df9b
aaahrenovskaya <yourally69@gmail.com>
2021-03-01 15:06:36 +0300

    feature: update to 0.2.43 обновлена функция dateToPlain + добавлены комментарии

--- projects/ng-project-helper/src/lib/functions/classTransform.ts
+++ projects/ng-project-helper/src/lib/functions/classTransform.ts
@@ -1,14 +1,33 @@
 import { dateToString } from './dateToString';
 
 
-export function dateToPlain(strFormat?: string): (value: Date) => string {
-  return value => value ? dateToString(value, strFormat) : null;
+function getFormatDate(value: Date, strFormat?: string | 'full'): string {
+  return strFormat === 'full' ? value.toString() : dateToString(value, strFormat);
 }
 
+/**
+ * Форматируем дату в строку, по умолчанию формат 'yyyy-MM-dd',
+ * чтобы сделать дату строкой, нужно вложить 'full'
+ * Используется для @Transform(dateToPlain(), { toPlainOnly: true })
+ *
+ * Все форматы можно посмотреть по ссылке https://date-fns.org/v2.17.0/docs/format
+ */
+export function dateToPlain(strFormat?: string | 'full'): (value: Date) => string {
+  return value => value ? getFormatDate(value, strFormat) : null;
+}
+
+/**
+ * Форматируем строку в дату.
+ * Используется для @Transform(dateToClass(), { toClassOnly: true })
+ */
 export function dateToClass(): (value: string) => Date {
   return value => value ? new Date(value) : null;
 }
-
+/**
+ * Форматируем объект в свойство.
+ * Пример @Transform(objectToField<DefaultApi, keyof DefaultApi>('id'), {toPlainOnly: true})
+ * Используется для @Transform()
+ */
 export function objectToField<T, K extends keyof T>(key: K): (value: T) => T[K] {
   return value => value ? value[key] : null;
 }
