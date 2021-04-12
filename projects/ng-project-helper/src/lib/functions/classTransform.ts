04b025a7c338858de365e6b80c4f234678b28204
alenahrenovskaya <yourally69@gmail.com>
2021-04-12 09:35:56 +0300

    feature: update to 0.2.47 objectArrayToFieldArray

--- projects/ng-project-helper/src/lib/functions/classTransform.ts
+++ projects/ng-project-helper/src/lib/functions/classTransform.ts
@@ -26,8 +26,18 @@ export function dateToClass(): (value: string) => Date {
 /**
  * Форматируем объект в свойство.
  * Пример @Transform(objectToField<DefaultApi, keyof DefaultApi>('id'), {toPlainOnly: true})
+ * Получим {... type: {id: 1, name: 'Name1'} ...} => {... type: 1 ...}
  * Используется для @Transform()
  */
 export function objectToField<T, K extends keyof T>(key: K): (value: T) => T[K] {
   return value => value ? value[key] : null;
+}
+/**
+ * Форматируем список объектов в список свойств.
+ * Пример @Transform(objectArrayToFieldArray<DirectionApi, 'id'>('id'), { toPlainOnly: true })
+ * Получим {... type: [{id: 1, name: 'Name1'}, {id: 2, name: 'Name2'}] ...} => {... type: [1, 2] ...}
+ * Используется для @Transform()
+ */
+export function objectArrayToFieldArray<T, K extends keyof T>(key: K): (value: T[]) => T[K][] {
+  return value => value ? value.map(item => item[key]) : null;
 }
