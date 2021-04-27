09aa37498f940aef6e9f428da841e49696de8071
yourally69@gmail.com <jbkhome69>
2021-04-27 16:05:44 +0300

    comment: directives

--- projects/ng-project-helper/src/lib/directives/stop-propagation/stop-propagation.directive.ts
+++ projects/ng-project-helper/src/lib/directives/stop-propagation/stop-propagation.directive.ts
@@ -10,7 +10,7 @@ import { Directive, HostListener } from '@angular/core';
  * Если в mat-form-field + mat-select указать `<button matSuffix (click)="clearSelect()">`
  * то после очистки поля, автоматически пойдет цепочка событий дальше, что приведет
  * к открытию списка опций у `<mat-select>` (это дефолтное поведение на любой метод (click)
- * вызыванный внутри mat-select.
+ * вызыванный внутри mat-select).
  *
  * Но если добавить директиву - `<button matSuffix (click)="clearSelect() stopPropagation">`,
  * то после очистки поля цепочка прервется, и mat-select не будет открываться для пользователя просто так.
