0ef53369a1e139057ccbdc1e595071a82287dc89
yourally69@gmail.com <jbkhome69>
2021-04-27 16:10:54 +0300

    comment: edited-option

--- projects/ng-project-helper/src/lib/edited-option/edited-option.component.ts
+++ projects/ng-project-helper/src/lib/edited-option/edited-option.component.ts
@@ -9,6 +9,11 @@ import { CustomOption } from '../classes/customOption';
   templateUrl: './edited-option.component.html',
   providers: [{provide: CustomOption, useExisting: EditedOptionComponent}]
 })
+/** Компонент для добавления к mat-option кнопки редактирования,
+ * при клике на которую в itemSubject передается значение из value.
+ * Удобно обрабатывать директивой и уже непосредственно в приложении
+ * писать свою логику - например вызывать необходимый диалог с формой редактирования;
+ */
 export class EditedOptionComponent<T> implements CustomOption<T> {
   @Input() value: T;
 
