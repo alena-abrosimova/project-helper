c64274bf9f3b0bc0e69b446431ef82b9e3d2b666
yourally69@gmail.com <jbkhome69>
2021-04-29 08:30:20 +0300

    comment: default-classes.ts

--- projects/ng-project-helper/src/lib/default-classes.ts
+++ projects/ng-project-helper/src/lib/default-classes.ts
@@ -1,17 +1,23 @@
 import { PaginationOption } from './classes/paginationOptions';
 
 
+/** Дефолтный класс с любыми ключами */
 export class DefaultStringChoices<T> {
   [key: string]: T;
 }
 
+/** Дефолтный класс с любыми ключами для значений типа string[],
+ * в системе используется для router.navigate(commands) для разных ролей
+ * например:
+ * - `const RolesRouterMap: DefaultUrls = {operator: ['app', 'base'], admin: ['app', 'admin']}`
+ */
 export class DefaultUrls {
   [key: string]: string[];
 }
-
+/** Дефолтный класс с любыми ключами для фильтрации и обязательными для BaseTableComponent */
 export class DefaultParams {
-    [key: string]: any;
+  [key: string]: any;
 
-    ordering?: string;
-    paginationOption?: PaginationOption;
+  ordering?: string;
+  paginationOption?: PaginationOption;
 }
